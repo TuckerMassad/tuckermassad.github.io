21ca71e7362131f3801f309be4763491df4d211a
Tucker Massad <tucker@imarc.com>
2019-02-14 15:14:48 -0500

    title style update 15

--- js/maintitle.js
+++ js/maintitle.js
@@ -1,11 +1,15 @@
 var sety;
 var colorArray = ['#61892F', '#86c232', '#222629', '#fff', '#686e70'];
 var mq =  window.matchMedia( "(min-width: 480px)" );
+var mqi = window.matchMedia( "(max-width: 960px)" );
 
 if (mq.matches) {
-	sety = -24;
+	sety = -28;
 }
-else {
+else if (mqi.matches) {
+	sety = -224;
+}
+else{
 	sety = -224;
 }
 
@@ -14,19 +18,18 @@ var burst = new mojs.Burst({
 	speed: -2,
 	opacity: 0.62,
 	y: sety,
-	radiusX: { 900: 10 },
+	radiusX: { 500: 10 },
 	radiusY: { 170: 10 },
 	duration: 4000,
 	count: 16,
 	children: {
 		shape: 'polygon',
 		points: 10,
-		scale: { 'rand(0, 3)': 0 },
+		scale: { 'rand(0, 2.8)': 0 },
 		fill: colorArray,
 		angle: { 'rand(0, 360)': 'rand(0, 360)' },
 		duration: 'rand(1000, 7000)',
 		delay: 'rand(0, 2000)' },
-		speed: 0.2,
 
 	onComplete: function onComplete() {
 		this.generate().replay();
