89a42a3c0681b37d05c271776f47388da2751325
Tucker Massad <tucker@imarc.com>
2019-02-14 15:06:07 -0500

    title style update 13

--- js/maintitle.js
+++ js/maintitle.js
@@ -1,21 +1,21 @@
 var sety;
 var colorArray = ['#61892F', '#86c232', '#222629', '#fff', '#686e70'];
-var mq =  window.matchMedia( "only screen and (max-width: 960px)" );
+var mq =  window.matchMedia( "(min-width: 480px)" );
 
 if (mq.matches) {
-	sety = -224;
+	sety = -24;
 }
 else {
-	sety = -28;
+	sety = -224;
 }
 
 var burst = new mojs.Burst({
 
 	speed: -2,
-	opacity: 0.58,
+	opacity: 0.62,
 	y: sety,
-	radiusX: { 800: 10 },
-	radiusY: { 160: 10 },
+	radiusX: { 900: 10 },
+	radiusY: { 170: 10 },
 	duration: 4000,
 	count: 16,
 	children: {
