c8bec7878443d20d6b219072f7c717fbb27d6a9f
Tucker Massad <tucker@imarc.com>
2019-07-09 15:14:52 -0400

    tuesday update 1

--- js/preload.js
+++ js/preload.js
@@ -6,26 +6,3 @@ paceOptions = {
     ajax: false,
     eventLag: false // disabled
 };
-
-var initDestroyTimeOutPace = function() {
-    var counter = 0;
-
-    var refreshIntervalId = setInterval( function(){
-        var progress;
-
-        if( typeof ( '.pace-progress' ).attr( 'data-progress-text' ) !== 'undefined' ) {
-            progress = Number( $( '.pace-progress' ).attr( 'data-progress-text' ).replace("%" ,'') );
-        }
-
-        if( progress === 99 ) {
-            counter++;
-        }
-
-        if( counter > 50 ) {
-            clearInterval(refreshIntervalId);
-            Pace.stop();
-        }
-    }, 100);
-}
-
-initDestroyTimeOutPace();
