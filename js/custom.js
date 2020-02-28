b14f2ae1081614ad59c32d45b9b3866fc2bd9360
Tucker <tucker@graftonstudio.com>
2020-02-27 20:05:06 -0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -20,7 +20,12 @@ $(document).ready(function() {
 	$("a.scroll[href^='#']").on('click', function(e) {
 		e.preventDefault();
 		var hash = this.hash;
-		$('html, body').animate({ scrollTop: $(this.hash).offset().top}, 1000, function(){window.location.hash = hash;});
+		if(hash == "#services"){
+			$('html, body').animate({ scrollTop: $(this.hash).offset().top - 40}, 1000);
+		}
+		else{
+			$('html, body').animate({ scrollTop: $(this.hash).offset().top}, 1000);
+		}
 	});
 	
 	$('#skills-toggle').click(function() {
